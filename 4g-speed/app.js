7f0fdf27f0efe2c4f1c597e60f42c320a96f150f
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2018-04-19 22:39:32 +0100

    Added jQuery fallback

--- 4g-speed/app.js
+++ 4g-speed/app.js
@@ -7,6 +7,14 @@
 
 // For more info on the LAA band, see here: https://en.wikipedia.org/wiki/LTE_in_unlicensed_spectrum
 
+// Fallback for jQuery
+if (!window.jQuery){
+	var j = document.createElement("script");
+	j.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+	j.type = "text/javascript";
+	document.head.append(j);
+}
+
 // Universal configurations
 var base = .5;							// Base number for multipliers
 var mod = [.5,1,1.5,1.95]; 				// Modulation Multiplier
